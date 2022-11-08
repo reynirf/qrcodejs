dbc15b40772f74e0a2882abd67e71c1a1c23b27a
Reynir Hauksson <reynirfreyr9@gmail.com>
2022-11-08 16:50:19 +0000

    More typings

--- qrcode.d.ts
+++ qrcode.d.ts
@@ -1,8 +1,17 @@
 declare class QRCode {
-  constructor() {}
+  constructor(element: string, options: BaseOptions) {}
   makeImage(): void;
   makeCode(text: string): void;
   clear(): void;
+}
+
+interface BaseOptions {
+  width?: number = 256;
+  height?: number = 256;
+  typeNumber?: number = 4;
+  colorDark?: string = '#000000';
+  colorLight?: string = '#ffffff';
+  correctLevel?: QRErrorCorrectLevel.H;
 }
 
 export default QRCode
