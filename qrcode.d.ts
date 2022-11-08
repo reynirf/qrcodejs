e0f1f7a9e3797896f9358cb096c0c3915f37538f
Reynir Hauksson <reynirfreyr9@gmail.com>
2022-11-08 16:54:35 +0000

    Test typings

--- qrcode.d.ts
+++ qrcode.d.ts
@@ -1,17 +1,17 @@
 declare class QRCode {
-  constructor(element: string, options: BaseOptions) {}
+  constructor(element: string, options: BaseOptions);
   makeImage(): void;
   makeCode(text: string): void;
   clear(): void;
 }
 
 interface BaseOptions {
-  width?: number = 256;
-  height?: number = 256;
-  typeNumber?: number = 4;
-  colorDark?: string = '#000000';
-  colorLight?: string = '#ffffff';
-  correctLevel?: QRErrorCorrectLevel.H;
+  width?: number;
+  height?: number;
+  typeNumber?: number;
+  colorDark?: string;
+  colorLight?: string;
+  correctLevel?: any;
 }
 
 export default QRCode
